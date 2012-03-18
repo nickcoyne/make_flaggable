2f02d5a2f45a970dd6165d42d249617633e84f81
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2012-03-17 22:24:21 -0400

    Added the with_flaggable scope.
    Allows flaggings to be found by flaggble.

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -3,5 +3,6 @@ module MakeFlaggable
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
+    scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
   end
 end
