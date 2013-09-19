9819f5c75df76633bf365bcbbaf9716bd3f65c37
Eric Berry <cavneb@gmail.com>
2013-09-19 06:37:08 -0600

    removed protected attributes

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -1,11 +1,32 @@
 module MakeFlaggable
   class Flagging < ActiveRecord::Base
-    include ActiveModel::MassAssignmentSecurity
-    
-    attr_accessible :flaggable, :flagger, :flag
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
+
+    def flaggable
+      @flaggable
+    end
+
+    def flaggable=(value)
+      @flaggable = value
+    end
+
+    def flagger
+      @flagger
+    end
+
+    def flagger=(value)
+      @flagger = value
+    end
+
+    def flag
+      @flag
+    end
+
+    def flag=(value)
+      @flag = value
+    end
   end
 end
