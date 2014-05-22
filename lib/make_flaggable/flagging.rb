b533bdc95611ab39c51a04f20313f95b67e3ff55
Michael Pavling <pavling@gmail.com>
2014-05-22 13:16:17 +0100

    properly removing the overloading methods in Flagging, and adding required attr_accessible attributes

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -4,29 +4,6 @@ module MakeFlaggable
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
-
-    # def flaggable
-    #   @flaggable
-    # end
-
-    # def flaggable=(value)
-    #   @flaggable = value
-    # end
-
-    # def flagger
-    #   @flagger
-    # end
-
-    # def flagger=(value)
-    #   @flagger = value
-    # end
-
-    # def flag
-    #   @flag
-    # end
-
-    # def flag=(value)
-    #   @flag = value
-    # end
+    attr_accessible  :flaggable, :flagger, :flag
   end
 end
