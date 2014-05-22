7d7dbf89da11de98f17a23e3d3173bebdb35a4ad
ScottyGams <ScottyGammon@gmail.com>
2014-05-22 14:20:07 +0100

    updated version. made attr accesible

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -4,6 +4,7 @@ module MakeFlaggable
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
+    
     attr_accessible  :flaggable, :flagger, :flag
   end
 end
