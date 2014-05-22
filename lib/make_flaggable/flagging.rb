155538f7868b3fc601f7c3b35ae51e0f2ab2b0de
Michael Pavling <pavling@gmail.com>
2014-05-22 12:27:51 +0100

    tweaking flagging class

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -5,28 +5,28 @@ module MakeFlaggable
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
 
-    def flaggable
-      @flaggable
-    end
+    # def flaggable
+    #   @flaggable
+    # end
 
-    def flaggable=(value)
-      @flaggable = value
-    end
+    # def flaggable=(value)
+    #   @flaggable = value
+    # end
 
-    def flagger
-      @flagger
-    end
+    # def flagger
+    #   @flagger
+    # end
 
-    def flagger=(value)
-      @flagger = value
-    end
+    # def flagger=(value)
+    #   @flagger = value
+    # end
 
-    def flag
-      @flag
-    end
+    # def flag
+    #   @flag
+    # end
 
-    def flag=(value)
-      @flag = value
-    end
+    # def flag=(value)
+    #   @flag = value
+    # end
   end
 end
