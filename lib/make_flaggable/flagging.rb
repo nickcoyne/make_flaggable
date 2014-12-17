6437118190c327c4de99873506efa2f0b858a06b
Nick <nick@codevader.com>
2014-12-18 07:24:45 +1300

    Add counter cache to flaggable

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -1,9 +1,9 @@
 module MakeFlaggable
   class Flagging < ActiveRecord::Base
-    belongs_to :flaggable, :polymorphic => true
-    belongs_to :flagger, :polymorphic => true
-    scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
-    scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
+    belongs_to :flaggable, polymorphic: true, counter_cache: true
+    belongs_to :flagger, polymorphic: true
+    scope :with_flag, lambda { |flag| where(flag: flag.to_s) }
+    scope :with_flaggable, lambda { |flaggable| where(:flaggable_type: flaggable.class.name, flaggable_id: flaggable.id) }
     
     attr_accessible  :flaggable, :flagger, :flag
   end
