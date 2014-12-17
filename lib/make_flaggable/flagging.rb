07583415e2de72e3a88d7acf21c3fb6886e82e7b
Nick <nick@codevader.com>
2014-12-18 07:26:48 +1300

    Fix typo

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -3,7 +3,7 @@ module MakeFlaggable
     belongs_to :flaggable, polymorphic: true, counter_cache: true
     belongs_to :flagger, polymorphic: true
     scope :with_flag, lambda { |flag| where(flag: flag.to_s) }
-    scope :with_flaggable, lambda { |flaggable| where(:flaggable_type: flaggable.class.name, flaggable_id: flaggable.id) }
+    scope :with_flaggable, lambda { |flaggable| where(flaggable_type: flaggable.class.name, flaggable_id: flaggable.id) }
     
     attr_accessible  :flaggable, :flagger, :flag
   end
